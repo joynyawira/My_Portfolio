ecf05ea2d49fa0ca0c7bbe8f09f1fae2699e42ca
Joy Nyawira <132754543+joynyawira@users.noreply.github.com>
2024-08-06 14:55:04 +0300

    updated code

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ function toggleMenu() {
   const navbar = document.querySelector('.navbar');
   navbar.classList.toggle('active');
 }
+
+// Close the menu when a link is clicked
+document.querySelectorAll('.navbar a').forEach(link => {
+  link.addEventListener('click', () => {
+    const navbar = document.querySelector('.navbar');
+    if (navbar.classList.contains('active')) {
+      navbar.classList.remove('active');
+    }
+  });
+});
