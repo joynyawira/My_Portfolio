36d047337def10dcad5430352409b75f9db47c52
Joy Nyawira <132754543+joynyawira@users.noreply.github.com>
2024-11-09 15:28:57 +0300

    updated code

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-function toggleMenu() {
-  const navbar = document.querySelector('.navbar');
-  navbar.classList.toggle('active');
-}
 
-// Close the menu when a link is clicked
-document.querySelectorAll('.navbar a').forEach(link => {
-  link.addEventListener('click', () => {
+  function toggleMenu() {
     const navbar = document.querySelector('.navbar');
-    if (navbar.classList.contains('active')) {
-      navbar.classList.remove('active');
-    }
+    navbar.classList.toggle('active');
+  }
+
+  // Close the menu when a link is clicked
+  document.querySelectorAll('.navbar a').forEach(link => {
+    link.addEventListener('click', () => {
+      const navbar = document.querySelector('.navbar');
+      if (navbar.classList.contains('active')) {
+        navbar.classList.remove('active');
+      }
+    });
   });
-});
